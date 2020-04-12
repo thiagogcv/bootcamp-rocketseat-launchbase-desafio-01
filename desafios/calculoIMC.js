95965ca00aff3cdb6c8f6027a44a12e183de3e2c
Thiago Costa <thiagogcv@gmail.com>
2020-04-12 18:44:40 -0300

    Create calculoIMC.js
    Calculo de IMC de um indivíduo

---
+++ desafios/calculoIMC.js
@@ -0,0 +1,16 @@
+const nome = "Carlos"
+const peso = 118
+const altura = 1.88
+
+const imc = peso / (altura * altura)
+
+if (imc >= 30) {
+
+    console.log(`${nome}, você esta acima do peso com ${imc.toFixed(2)} de massa corporal. Vá a um nutricionista`)
+
+
+} else {
+
+    console.log(`${nome}, você não está acima peso com ${imc.toFixed(2)} de massa corporal. Continue se cuidando`);
+
+}
