c1a266eae21222d404139dbc0f53cea655d87064
Thiago Costa <thiagogcv@gmail.com>
2020-04-12 18:46:22 -0300

    Update calculoIMC.js

--- desafios/calculoIMC.js
+++ desafios/calculoIMC.js
@@ -8,7 +8,6 @@ if (imc >= 30) {
 
     console.log(`${nome}, você esta acima do peso com ${imc.toFixed(2)} de massa corporal. Vá a um nutricionista`)
 
-
 } else {
 
     console.log(`${nome}, você não está acima peso com ${imc.toFixed(2)} de massa corporal. Continue se cuidando`);
