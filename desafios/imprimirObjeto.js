ce6411c9689da4681e0b629d9844770eee4b6d3e
Thiago Costa <thiagogcv@gmail.com>
2020-04-13 16:52:13 -0300

    Create imprimirObjeto.js

---
+++ desafios/imprimirObjeto.js
@@ -0,0 +1,13 @@
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programação",
+    endereço: {
+        rua: "Rua Guilherme Gembala",
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada na ${empresa.endereço.rua}, ${empresa.endereço.numero}`);
+
+
