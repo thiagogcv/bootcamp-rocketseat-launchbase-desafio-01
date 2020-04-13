490657d6b898208d22365ba19dba9bb0ed9122f5
Thiago Costa <thiagogcv@gmail.com>
2020-04-13 17:44:43 -0300

    Update vetores-objetos.js
    Adicionando novo usuario ao programa

--- desafios/vetores-objetos.js
+++ desafios/vetores-objetos.js
@@ -5,17 +5,28 @@ Um programador pode trabalhar com várias tecnologias, por isso armazene essas t
 Imprima em tela o nome e especialidade da primeira tecnologia que o usuário utiliza
 **/
 
-const usuario = {
-    nome: "Diego",
-    idade: 25,
+const usuario = [
+    {
+        nome: "Diego",
+        idade: 25,
 
-    tecnologias: [
-        { nome: "C++", especialidade: "Desktop" },
-        { nome: 'Python', especialidade: 'Data Science' },
-        { nome: 'JavaScript', especialidade: 'Web/Mobile' }
-    ]
-}
+        tecnologias: [
+            { nome: "C++", especialidade: "Desktop" },
+            { nome: 'Python', especialidade: 'Data Science' },
+            { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+        ]
+    },
+    {
+        nome: "Thiago",
+        idade: 36,
 
+        tecnologias: [
+            { nome: "Java", especialidade: "Web" },
+            { nome: 'MySQL', especialidade: 'Data Base' },
+            { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+        ]
+    }
+]
 
-console.log(`O usuário ${usuario.nome}, tem ${usuario.idade} anos e trabalha com ${usuario.tecnologias[0].nome}, com a especialidade em ${usuario.tecnologias[0].especialidade}`)
+console.log(`O usuário ${usuario[0].nome}, tem ${usuario[0].idade} anos e trabalha com ${usuario[0].tecnologias[0].nome}, com a especialidade em ${usuario[0].tecnologias[0].especialidade}. Quanto o ${usuario[1].nome}, tem ${usuario[1].idade} anos, e trabalha com ${usuario[1].tecnologias[0].nome}, com especialidade em ${usuario[1].tecnologias[0].especialidade}`)
 
