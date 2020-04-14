84c2d0b90e744a082831c8e7d3c12a67e2918e88
Thiago Costa <thiagogcv@gmail.com>
2020-04-14 14:06:25 -0300

    Update vetores-objetos.js

--- desafios/vetores-objetos.js
+++ desafios/vetores-objetos.js
@@ -21,12 +21,22 @@ const usuario = [
         idade: 36,
 
         tecnologias: [
-            { nome: "Java", especialidade: "Web" },
+            { nome: "Java", especialidade: "Desktop" },
             { nome: 'MySQL', especialidade: 'Data Base' },
+            { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+        ]
+    },
+    {
+        nome: "Carlos",
+        idade: 28,
+
+        tecnologias: [
+            { nome: "Java", especialidade: "Desktop" },
+            { nome: 'PHP', especialidade: 'Web' },
             { nome: 'JavaScript', especialidade: 'Web/Mobile' }
         ]
     }
 ]
 
-console.log(`O usuário ${usuario[0].nome}, tem ${usuario[0].idade} anos e trabalha com ${usuario[0].tecnologias[0].nome}, com a especialidade em ${usuario[0].tecnologias[0].especialidade}. Quanto o ${usuario[1].nome}, tem ${usuario[1].idade} anos, e trabalha com ${usuario[1].tecnologias[0].nome}, com especialidade em ${usuario[1].tecnologias[0].especialidade}`)
+console.log(`O usuário ${usuario[0].nome}, tem ${usuario[0].idade} anos e trabalha com ${usuario[0].tecnologias[0].nome}, com a especialidade em ${usuario[0].tecnologias[0].especialidade}. Quanto o usuário ${usuario[1].nome}, tem ${usuario[1].idade} anos, e trabalha com ${usuario[1].tecnologias[0].nome}, com especialidade em ${usuario[1].tecnologias[0].especialidade}. Já o usuário ${usuario[2].nome}, tem ${usuario[2].idade} anos e trabalha com ${usuario[2].tecnologias[1].nome} com especialidade na ${usuario[2].tecnologias[1].especialidade}.`)
 
