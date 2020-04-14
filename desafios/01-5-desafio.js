a2aa5084bb9a2e765592de0c7c12ed25ab120d92
Thiago Costa <thiagogcv@gmail.com>
2020-04-14 19:16:35 -0300

    Update 01-5-desafio.js

--- desafios/01-5-desafio.js
+++ desafios/01-5-desafio.js
@@ -1,11 +1,11 @@
 const usuarios = [
-    { nome: "Carlos", tecnologias: ["HTML", " CSS"] },
-    { nome: "Jasmine", tecnologias: ["JavaScript", " CSS"] },
-    { nome: "Tuane", tecnologias: ["HTML", " Node.js"] }
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
 ]
 
     for (let i = 0; i < usuarios.length; i++) {
-      console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+      console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias.join(", ")}`)
 
     }
 
