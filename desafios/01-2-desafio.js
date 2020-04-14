d2d5a3f2fffd3c0fb3e757a6d994c0e9f8186d7c
Thiago Costa <thiagogcv@gmail.com>
2020-04-14 19:01:02 -0300

    Cálculo de aposentadoria

--- desafios/01-2-desafio.js
+++ desafios/01-2-desafio.js
@@ -1,22 +1,3 @@
-/**
- * CALCULO DE APOSENTADORIA
-Crie um programa para calcular a aposentadoria de uma pessoa.
-
-Obs.: Esse cálculo é fictício, dentro da aposentadoria 
-existem muitos outros fatores para serem levados em conta
-
-Baseado nos valores das fórmulas a seguir para calcular 
-se a pessoa está apta ou não para se aposentar e no fim 
-imprima uma mensagem em tela.
-
-O tempo de contribuição mínimo para homens é de 35 anos e, 
-para mulheres, 30 anos;
-Utilizando a regra 85-95, 
-a soma da idade com o tempo de contribuição do homem 
-precisa ser de no mínimo 95 anos, 
-enquanto a mulher precisa ter no mínimo 85 anos na soma;
- */
-
 const nome = "Catia"
 const sexo = "F"
 const idade = 66
